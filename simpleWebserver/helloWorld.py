de3512f5b07e30b6836fd617e9fe0c2ed78f5ad7
LubomirJagos <lubomir.jagos@honeywell.com>
2020-10-31 05:38:47 +0100

    Added interactive oscilloscope Hantek 6022, on Raspi or PC must be Hantek 6022 connected to run this.
    Result displayed at url:
        /oscilloscope?timeToRead=0.4       <---- this returns samples
        /showScope                         <---- interactive GUI
    
    NOT RUNNING:
         - change sampleRate        - making crash and unable use command capture to continue control scope
    
    RUNNING:
          - scope reading samples at stable Fs=20kS

--- simpleWebserver/helloWorld.py
+++ simpleWebserver/helloWorld.py
@@ -1,12 +1,18 @@
 import os
 import numpy as np
+import subprocess
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import csv
+import sys
 
 from flask import Flask, request
 
 
-currDir = os.path.abspath(__file__)
+currDir = os.path.dirname(os.path.abspath(__file__))
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 def index():
 	indexFile = open(os.path.dirname(os.path.abspath(__file__)) + "/index.html")
@@ -23,6 +29,46 @@ def deg2rad():
 	returnMsg = "User input degrees to radians: " + str(np.deg2rad(int(data)))
 	#returnMsg += "<br /><a href='/'>&lt;Back</a>"
 	return returnMsg
+	
+@app.route('/showScope')
+def showScope():
+	f = open(os.path.dirname(os.path.abspath(__file__)) + "/showScope.html")
+	return f.read()
+
+@app.route('/oscilloscope', methods=['GET'])
+def readHantek6022():
+	timeToRead = request.args.get('timeToRead')
+	if (timeToRead is None):
+		timeToRead = 0.001
+	sampleRate = request.args.get('sampleRate')
+	if (sampleRate is None):
+		sampleRate = 20
+
+	#scopeData = subprocess.check_output(['capture_6022.py','-t '+str(timeToRead), '-r '+str(sampleRate)])	
+	scopeData = subprocess.check_output(['capture_6022.py','-t '+str(timeToRead)])	
+
+	f = open("captured_data.csv", "w")
+	f.write(str(scopeData, 'utf-8'))
+	f.close()
+
+	x = []
+	y = []
+	with open('captured_data.csv','r') as csvfile:
+		plots = csv.reader(csvfile, delimiter=',')
+		for row in plots:
+			x.append(float(row[0]))
+			y.append(float(row[1]))
+
+	
+	plt.clf()
+	plt.plot(x,y, label='LuboJGraph')
+	figname = ''.join([currDir + '/static/captured_image.png'])
+	plt.savefig(figname)
+	
+	return scopeData
+	
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True, port=80, host='0.0.0.0')
